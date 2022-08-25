ca2bccc58871e3f5ce85ea8cbcac37fbf81d0605
Chibuike Okoye <talk2chibyke@yahoo.com>
2022-08-25 18:22:21 +1000

    finished Html and started writing Dom navigation variables for the logic

---
+++ assets/logic.js
@@ -0,0 +1,11 @@
+var currentquestionnumber = 0;
+var time = questions.length * 10;
+
+var questions = document.getElementById("questions");
+var options = document.getElementById("options");
+var startbtn = document.getElementById("start");
+var submitbtn = document.getElementById('submit');
+var timer = document.getElementById('timer');
+var initials = document.getElementById('initials');
+
+
